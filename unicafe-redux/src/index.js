7bf62a7b883ef96f1a4999f4d2607135ee1f30e6
William Ho <william.ho@nix-digital.com>
2022-03-01 14:45:02 +0800

    being 6.3

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <div>
       <button onClick={good}>good</button> 
-      <button onClick={ok}>ok</button> 
+      <button onClick={ok}>ok</button>
       <button onClick={bad}>bad</button>
       <button onClick={zero}>reset stats</button>
       <div>good {store.getState().good}</div>
